1e1c1e3c899cf0daa386d8cdf32299d493d42a23
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-05-26 13:14:55 +0800

    Initial staging status

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Todo struct {
+	ID        int    `json:"_id" bson:"_id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
+var collection *mongo.Collection
+
+func main() {
+	fmt.Println("Hello world")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("error loading .env file", err)
+	}
+
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to Mongodb atlas")
+}
