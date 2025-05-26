478681e8935bfa772d63876ddb9c36fa927d8e66
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-05-26 13:57:55 +0800

    Added create todo

--- main.go
+++ main.go
@@ -9,14 +9,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Todo struct {
-	ID        int    `json:"_id" bson:"_id"`
-	Completed bool   `json:"completed"`
-	Body      string `json:"body"`
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Completed bool               `json:"completed"`
+	Body      string             `json:"body"`
 }
 
 var collection *mongo.Collection
@@ -50,7 +51,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/api/todos", getTodos)
-	//app.Get("/api/todos", getTodos)
+	app.Post("/api/todos", createTodo)
 	//app.Get("/api/todos", getTodos)
 	//app.Get("/api/todos", getTodos)
 	//
@@ -79,4 +80,38 @@ func getTodos(c *fiber.Ctx) error {
 		todos = append(todos, todo)
 	}
 	return c.JSON(todos)
+}
+
+func createTodo(c *fiber.Ctx) error {
+	todo := new(Todo)
+
+	if err := c.BodyParser(todo); err != nil {
+		log.Printf("Error parsing request body: %v", err)
+
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid reqeust body"})
+	}
+
+	todo.ID = primitive.NilObjectID
+
+	if todo.Body == "" {
+		return c.Status(400).JSON(fiber.Map{"error": "Todo body cannot be empty"})
+	}
+
+	inserResult, err := collection.InsertOne(context.Background(), todo)
+	if err != nil {
+		if mongo.IsDuplicateKeyError(err) {
+			log.Printf("Duplicate key error during todo creation %v", err)
+			return c.Status(fiber.StatusConflict).JSON(fiber.Map{"error": "A todo with this ID aready exist"})
+		}
+		log.Printf("Error inserting todo into the databse %v", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to create todo because of databse error"})
+	}
+
+	if objectID, ok := inserResult.InsertedID.(primitive.ObjectID); ok {
+		todo.ID = objectID
+	} else {
+		log.Printf("Inserted ID is not of type primitive.ObjectID: %v", err)
+	}
+
+	return c.Status(201).JSON(todo)
 }
